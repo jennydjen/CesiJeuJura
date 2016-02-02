5b9d94c7874aed911e502d859dad41db64da0a6b
Jennifer Chaignepain <chaignepain.jennifer@gmail.com>
2016-02-02 21:03:07 +0100

    Ajout de la requete manquante

--- src/com/example/cesijeujura/Entities/Composant.java
+++ src/com/example/cesijeujura/Entities/Composant.java
@@ -6,39 +6,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 @Entity
+@NamedQueries(value = { @NamedQuery(name = "composant.findAll", query = "Select c from Compoosant c"), })
 public class Composant {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nom;
-	
-	@OneToMany(mappedBy="composant")
+
+	@OneToMany(mappedBy = "composant")
 	private List<Quantite_Composant_Module> modules;
-	
+
 	public Composant() {
 
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public List<Quantite_Composant_Module> getModules() {
 		return modules;
 	}
-	
+
 	@Override
 	public String toString() {
 		return nom;
