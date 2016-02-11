3a2e763906bb323df979de2bbc00cca28c7bf2ef
Jennifer Chaignepain <chaignepain.jennifer@gmail.com>
2016-02-11 22:18:36 +0100

    Correction pour récupérer un fichier

--- src/com/example/cesijeujura/IHM/popup/ChooseFile.java
+++ src/com/example/cesijeujura/IHM/popup/ChooseFile.java
@@ -1,5 +1,8 @@
 package com.example.cesijeujura.IHM.popup;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 import com.example.cesijeujura.Entities.Devis;
@@ -7,10 +10,16 @@ import com.example.cesijeujura.Entities.Fichier;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.FailedEvent;
+import com.vaadin.ui.Upload.FinishedEvent;
+import com.vaadin.ui.Upload.StartedEvent;
+import com.vaadin.ui.Upload.SucceededEvent;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Notification.Type;
 
-public class ChooseFile extends Window{
+public class ChooseFile extends Window implements Upload.StartedListener,
+		Upload.ProgressListener, Upload.FailedListener,
+		Upload.SucceededListener, Upload.FinishedListener {
 
 	public ChooseFile() {
 		setWidth(600.0f, Unit.PIXELS);
@@ -18,24 +27,61 @@ public class ChooseFile extends Window{
 		setModal(true);
 		FormLayout layout = new FormLayout();
 		layout.setMargin(true);
-		
-		layout.addComponent(new Upload("File", new Upload.Receiver() {
 
-            public OutputStream receiveUpload(String filename, String MIMEType) {
-            	// TODO : faire le code pour récupérer le fichier et le mettre dans le tableau
-//            	Fichier fichier = new Fichier();
-//            	Devis devis = new Devis();
-//            	fichier.setDevis(devis.getId());
-//            	fichier.setPath(filename);
-//            	//fichier.u
-            	
-            	Notification.show("Récupérer le fichier "+ filename,
-						Type.TRAY_NOTIFICATION);
-            	close();
-                return null;
-            }
-        }));
+		Upload upload = new Upload("File", new Upload.Receiver() {
+
+			public OutputStream receiveUpload(String filename, String MIMEType) {
+
+				try {
+					return new FileOutputStream(filename);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return null;
+				}
+			}
+		});
+		
+		layout.addComponent(upload);
+		
+		 upload.addStartedListener(this);
+         upload.addProgressListener(this);
+         upload.addFailedListener(this);
+         upload.addSucceededListener(this);
+         upload.addFinishedListener(this);
 
 		setContent(layout);
+	}
+
+	@Override
+	public void uploadFinished(FinishedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uploadSucceeded(SucceededEvent event) {
+		// TODO : faire le code pour récupérer le fichier et le mettre		
+		Notification.show("Récupérer le fichier "+ event.getFilename(),
+				Type.TRAY_NOTIFICATION);
+		close();
+	}
+
+	@Override
+	public void uploadFailed(FailedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateProgress(long readBytes, long contentLength) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void uploadStarted(StartedEvent event) {
+		// TODO Auto-generated method stub
+
 	}
 }
